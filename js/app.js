e6bece28b100c324608be8d79edd72c278bb7062
Hoang-26-09 <sun260907@gmail.com>
2026-08-02 21:44:22 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -91,7 +91,7 @@ class FirebaseManager {
     }
 
     async isAdmin(uid) {
-        return uid === '5852621653' || uid === 'test123';
+        return uid === '5852621653' || uid === ' ';
     }
 
     async dailyCheckin(uid) {
